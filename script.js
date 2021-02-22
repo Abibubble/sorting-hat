cd83620c625c2000b96d563220ae3e304bb98301
Abibubble <abi.yamaha.neo@gmail.com>
2021-02-22 20:49:17 +0000

    wip: Add to boilerplate

---
+++ script.js
@@ -0,0 +1,3 @@
+// On click, sorting hat disappears, revealing houses. cycle around houses, greying out each one in order
+// Alight on a house at random, pause, then display house name, crest, and info about house
+// Restart button clears the house and goes back to the sorting hat.
